9a3952c6c242f328c1b498b82b51d50baa4aa336
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-06-07 13:58:09 -0700

    adds fetch request to shopping list

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,12 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -20,10 +26,7 @@ function ShoppingList() {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter
-        category={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
+      <Filter category={selectedCategory} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} item={item} />
